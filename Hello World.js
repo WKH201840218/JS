847aa2676678a54c3d8fbff7f7fdbf5a0fe0e8f8
wkh005676 <80237099+WKH201840218@users.noreply.github.com>
2021-04-01 22:13:39 +0900

    Add files via upload

---
+++ Hello World.js
@@ -0,0 +1,23 @@
+// let date = new Date();
+// let hours = date.getHours();
+
+// if (hours < 10){
+//      console.log("JS 테스트1");
+// } else {
+//     if (hours < 20){
+//     console.log("JS 테스트2");
+//     } else {
+//         console.log("JS 테스트3");
+//     }
+// }
+//
+let input = 30;
+
+switch (input % 2) {
+    case 0:
+        console.log("스위치 짝");
+        break;
+    case 1:
+        console.log("스위치 홀");
+        break;
+}
